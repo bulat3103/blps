047cc8d10ff633a2dc0b5ed1269a2276f2b50697
admin <hafizov.bulat2002@gmail.com>
2023-02-23 16:16:24 +0300

    Added sql script

---
+++ src/main/resources/sql/create-tables.sql
@@ -0,0 +1,48 @@
+create table tests
+(
+    id bigint primary key not null,
+    name text not null,
+    points_sum int not null default 0,
+    points_count int not null default 0
+);
+
+create table questions
+(
+    id bigint primary key not null,
+    text text not null,
+    answers text not null
+);
+
+create table test_question
+(
+    test_id bigint not null references tests(id),
+    q_id bigint not null references questions(id),
+    number int not null,
+    primary key (test_id, q_id)
+);
+
+create table answer
+(
+    q_id bigint not null references questions(id),
+    ans_num int not null,
+    rate int not null,
+    primary key (q_id, ans_num)
+);
+
+create table test_result
+(
+    id bigint primary key not null,
+    test_id bigint not null references tests(id),
+    left_bound int not null,
+    right_bound int not null,
+    description text not null
+);
+
+create table comments
+(
+    id bigint primary key not null,
+    test_id bigint not null references tests(id),
+    write_date timestamp not null,
+    writer text,
+    comment text not null
+);
