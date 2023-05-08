ba98c49dbfa1a1e2239bcb4bf8cfcb12c1110351
admin <hafizov.bulat2002@gmail.com>
2023-05-08 12:58:12 +0300

    Connected MailService to Cloud

--- mail-service/src/main/java/com/example/mailservice/MailServiceApplication.java
+++ mail-service/src/main/java/com/example/mailservice/MailServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.mailservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJms
+@EnableDiscoveryClient
 public class MailServiceApplication {
 
     public static void main(String[] args) {
